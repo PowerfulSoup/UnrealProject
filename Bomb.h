ea5820a9ab2fe9d6edb28b14d69a37008cc81432
PowerfulSoup <67433736+PowerfulSoup@users.noreply.github.com>
2020-10-15 17:05:42 -0400

    Create Bomb.h

---
+++ Bomb.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Tool.h"
+#include "Bomb.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RPG_API ABomb : public ATool
+{
+	GENERATED_BODY()
+
+
+public:
+	ABomb();
+
+
+protected:
+
+	virtual void BeginPlay() override;
+
+public:
+
+	virtual void OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) override;
+
+	virtual void OnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex) override;
+
+	virtual void PrimaryFunction() override;
+	
+	virtual void SecondaryFunction() override;
+
+	virtual void Equip(class AMain* Char) override;
+
+	virtual void SetInstigator(AController* Inst) override;
+};
