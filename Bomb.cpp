1f88b763163a892975484eca86039030dd755856
PowerfulSoup <67433736+PowerfulSoup@users.noreply.github.com>
2020-11-30 20:05:13 -0500

    Update Bomb.cpp

--- Bomb.cpp
+++ Bomb.cpp
@@ -3,12 +3,38 @@
 
 #include "Bomb.h"
 #include "Main.h"
+#include "Enemy.h"
 #include "Engine/SkeletalMeshSocket.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+#include "Engine/World.h"
+#include "Kismet/GameplayStatics.h"
+#include "GameFramework/PlayerController.h"
+#include "Components/SphereComponent.h"
 
 
 ABomb::ABomb()
 {
+	//ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("ProjectileMovement"));
+	//ProjectileMovement->ProjectileGravityScale = 0.f;
+	//ProjectileMovement->bShouldBounce = true;
 
+	SkeletalMesh->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Block);
+	SkeletalMesh->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Overlap);
+	SkeletalMesh->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+
+	SkeletalMesh->SetMassOverrideInKg(FName("SM_Gruntling_BombDrop_Internal"), 5000.f, true);
+
+	ExplosionRadius = CreateDefaultSubobject<USphereComponent>(TEXT("ExplosionRadius"));
+	ExplosionRadius->SetupAttachment(GetRootComponent());
+	ExplosionRadius->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Ignore);
+	ExplosionRadius->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Overlap);
+	ExplosionRadius->SetSphereRadius(175.f);
+
+	Damage = 50.f;
+
+	ToolName = "Bombs";
+
+	bIsEquippedInRightHand = true;
 }
 
 void ABomb::BeginPlay()
@@ -22,6 +48,14 @@ void ABomb::OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* Oth
 {
 	Super::OnOverlapBegin(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex, bFromSweep, SweepResult);
 
+	if (OtherActor && bIsActive)
+	{
+		AEnemy* Enemy = Cast<AEnemy>(OtherActor);
+		if (Enemy)
+		{
+			Detonate();
+		}
+	}
 }
 
 void ABomb::OnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
@@ -29,23 +63,58 @@ void ABomb::OnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* Other
 
 }
 
-void ABomb::PrimaryFunction()
+void ABomb::PrimaryFunction()///THROW
 {
+	Super::PrimaryFunction();
 
+	
+	const FVector Rotation = ToolOwner->GetActorForwardVector();
+
+
+	SkeletalMesh->SetSimulatePhysics(true);
+	SkeletalMesh->DetachFromComponent(FDetachmentTransformRules::KeepWorldTransform);
+	SkeletalMesh->AddImpulse(Rotation,"SM_Gruntling_BombDrop_Internal",true);
+	//SkeletalMesh->AddForce(Rotation, "SM_Gruntling_BombDrop_Internal", true);
+	bIsActive = true;
+
+	BeginCountDown();
 }
 
-void ABomb::SecondaryFunction()
+void ABomb::SecondaryFunction()//DROP
 {
+	SkeletalMesh->SetSimulatePhysics(true);
+	SkeletalMesh->DetachFromComponent(FDetachmentTransformRules::KeepWorldTransform);
 
+	BeginCountDown();
 }
 
 void ABomb::Equip(AMain* Char)
 {
 	Super::Equip(Char);
-
 }
 
 void ABomb::SetInstigator(AController* Inst)
 {
 	Super::SetInstigator(Inst);
+}
+
+void ABomb::BeginCountDown()
+{
+	GetWorld()->GetTimerManager().SetTimer(FuseTimer, this, &ABomb::Detonate, 4.f, false);
+
+}
+
+void ABomb::Detonate()
+{
+	UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), OverlapParticles,GetActorLocation());
+
+	TArray<AActor*> CharactersToDamage;
+	ExplosionRadius->GetOverlappingActors(CharactersToDamage,ACharacter::StaticClass());
+
+	for (AActor* Char : CharactersToDamage)
+	{
+		UGameplayStatics::ApplyDamage(Char, Damage, ToolInstigator, this, DamageTypeClass);
+	}
+
+	Destroy();
 }
