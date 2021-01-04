a2749ffa38ea5833fae63efb5b35aea194f918d0
PowerfulSoup <67433736+PowerfulSoup@users.noreply.github.com>
2021-01-04 16:44:27 -0500

    Create HitSwitchOpenDoor.h

---
+++ HitSwitchOpenDoor.h
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "HitSwitch.h"
+#include "HitSwitchOpenDoor.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RPG_API AHitSwitchOpenDoor : public AHitSwitch
+{
+	GENERATED_BODY()
+	
+public:
+	AHitSwitchOpenDoor();
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Door")
+		TArray<class ADoor*> DoorsToOpen;
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void SwitchFunction() override;
+
+	virtual void ToggleMeshMaterial() override;
+
+	virtual void ToggleReadyStatus() override;
+
+	virtual void ReadyTimer() override;
+
+};
