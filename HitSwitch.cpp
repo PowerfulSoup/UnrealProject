f5e472ba6ceb215797a12261e358e9bbd815a621
PowerfulSoup <67433736+PowerfulSoup@users.noreply.github.com>
2021-01-04 16:43:06 -0500

    Update HitSwitch.cpp

--- HitSwitch.cpp
+++ HitSwitch.cpp
@@ -3,6 +3,9 @@
 
 #include "HitSwitch.h"
 #include "Components/StaticMeshComponent.h"
+#include "Weapon.h"
+#include "Materials/Material.h"
+#include "Engine/World.h"
 
 // Sets default values
 AHitSwitch::AHitSwitch()
@@ -11,18 +14,26 @@ AHitSwitch::AHitSwitch()
 	PrimaryActorTick.bCanEverTick = true;
 
 	TargetMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Target Mesh"));
-	TargetMesh->BodyInstance.SetCollisionProfileName("BlockAllDynamic");
-
+	TargetMesh->SetCollisionProfileName(FName("Pawn"));
+	TargetMesh->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Overlap);
+	TargetMesh->SetCollisionResponseToChannel(ECollisionChannel::ECC_Camera, ECollisionResponse::ECR_Ignore);
+	TargetMesh->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Ignore);
 	RootComponent = TargetMesh;
 
+	SwitchTimerDuration = 1.f;
 
+	bSwitchedOn = false;
+	bRotate = false;
+	bCanToggle = false;
+	bReady = true;
 }
 
 // Called when the game starts or when spawned
 void AHitSwitch::BeginPlay()
 {
 	Super::BeginPlay();
-	TargetMesh->OnComponentHit.AddDynamic(this, &AHitSwitch::OnHit);
+	TargetMesh->SetMaterial(0, HitSwitchMaterialOff);
+
 }
 
 // Called every frame
@@ -30,10 +41,44 @@ void AHitSwitch::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	if (bRotate)
+	{
+		FRotator Rotation = GetActorRotation();
+		Rotation.Yaw += DeltaTime * 45.f;
+		SetActorRotation(Rotation);
+	}
 }
 
-void AHitSwitch::OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit)
+void AHitSwitch::SwitchFunction()
 {
 
 }
 
+void AHitSwitch::ToggleMeshMaterial()
+{
+	if (bSwitchedOn)
+	{
+		TargetMesh->SetMaterial(0, HitSwitchMaterialOff);
+	} 
+	else
+	{
+		TargetMesh->SetMaterial(0, HitSwitchMaterialOn);
+	}
+}
+
+void AHitSwitch::ReadyTimer()
+{
+	GetWorld()->GetTimerManager().SetTimer(SwitchTimer, this, &AHitSwitch::ToggleReadyStatus, SwitchTimerDuration, false);
+}
+
+void AHitSwitch::ToggleReadyStatus()
+{
+	if (!bReady)
+	{
+		bReady = true;
+	}
+	else
+	{
+		bReady = false;
+	}
+}
